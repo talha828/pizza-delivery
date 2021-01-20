13c0cedbf3548a6be316367b8d72da232ac96f3c
talha iqbal <61588132+talha828@users.noreply.github.com>
2021-01-20 11:26:56 +0500

    Add files via upload

---
+++ file.js
@@ -0,0 +1,51 @@
+
+function talha(){ 
+    var b="<h2>your order is</h2><h1>one Fast Food Pizza </h1><h2>your bill is</h2><h1>3500+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha1(){ 
+     var b="<h2>your order is</h2><h1>one Cheese-Stuffed Crust pizza </h1><h2>your bill is</h2><h1>3000+tax</h1> "
+    document.getElementById("showtext").innerHTML=(b);
+}
+function talha2(){ 
+     var b="<h2>your order is</h2><h1>one Pizza Bagels</h1><h2>your bill is</h2><h1>2500+tax</h1> "
+    document.getElementById("showtext").innerHTML=(b);
+}
+function talha3(){ 
+    var b="<h2>your order is</h2><h1>one Flatbread pizza</h1><h2>your bill is</h2><h1>2300+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha4(){ 
+    var b="<h2>your order is</h2><h1>one Thin Crust pizza </h1><h2>your bill is</h2><h1>2000+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha5(){ 
+    var b="<h2>your order is</h2><h1>one Sicilian Style Pizza </h1><h2>your bill is</h2><h1>1800+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha5(){ 
+    var b="<h2>your order is</h2><h1>one Chicago Deep Dish Pizza </h1><h2>your bill is</h2><h1>1500+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha7(){ 
+    var b="<h2>your order is</h2><h1> one Neapolitan Crust Pizza </h1><h2>your bill is</h2><h1>1200+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function talha8(){ 
+    var b="<h2>your order is</h2><h1>one NY Style Pizza  </h1><h2>your bill is</h2><h1>1000+tax</h1> "
+   document.getElementById("showtext").innerHTML=(b);
+}
+function done(){
+    alert("your ordre is successful")
+}
+
+
+        
+
+
+    
+
+
+
+
+
